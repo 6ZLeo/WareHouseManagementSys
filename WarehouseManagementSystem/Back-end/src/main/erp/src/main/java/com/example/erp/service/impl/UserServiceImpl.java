3b2eb8d440f1bf42021fbc73161752ecaf9cb9f1
link0812 <3200809604@qq.com>
2024-05-19 12:54:36 +0800

    更新了 ProductController的单元测试

--- WarehouseManagementSystem/Back-end/src/main/erp/src/main/java/com/example/erp/service/impl/UserServiceImpl.java
+++ WarehouseManagementSystem/Back-end/src/main/erp/src/main/java/com/example/erp/service/impl/UserServiceImpl.java
@@ -14,7 +14,7 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
-public class UserServiceImpl implements UserService {
+public abstract class UserServiceImpl implements UserService {
     @Autowired
     private  UserRepository userRepository;
 
