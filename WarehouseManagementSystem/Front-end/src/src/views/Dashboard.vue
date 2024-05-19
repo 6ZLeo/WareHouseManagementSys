c50855e4bb84c67a06eadb75977401ec777aacca
6ZLeo <136341102+6ZLeo@users.noreply.github.com>
2024-05-19 22:33:14 +0800

    重做侧边栏

--- WarehouseManagementSystem/Front-end/src/src/views/Dashboard.vue
+++ WarehouseManagementSystem/Front-end/src/src/views/Dashboard.vue
@@ -1,103 +1,138 @@
-<!-- Dashboard.vue -->
 <template>
-  <!-- 整体布局容器 -->
-  <div class="dashboard-container">
-    <!-- 背景图片 -->
-    <div class="background-image"></div>
+  <div class="sidebar">
+    <div class="sidebar-content">
+      <!-- Logo 或标题 -->
+      <div class="sidebar-header">
+        <img src="@/assets/logo.png" alt="Logo" class="sidebar-logo">
+      </div>
+      <div>
+        <h1 class="sidebar-title">仓库管理系统</h1>
+      </div>
+      <!-- 菜单项 -->
+      <el-menu
+        default-active="1"
+        class="el-menu-vertical-demo"
+        :collapse="isCollapsed"
+        background-color="#f0f2f5"
+        text-color="#303133"
+        active-text-color="#409eff"
+      >
+        <el-menu-item index="1" @click="handleMenuClick('/dashboard/myinfo')">
+          <i class="el-icon-user"></i>
+          <span slot="title" style="font-size: 20px;">我的信息</span>
+        </el-menu-item>
+        <el-menu-item index="5" @click="handleMenuClick('/dashboard/auto')">
+          <i class="el-icon-connection"></i>
+          <span slot="title" style="font-size: 20px;">自动化仓库</span>
+        </el-menu-item>
+        <el-menu-item index="3" @click="handleMenuClick('/dashboard/inventory')">
+          <i class="el-icon-suitcase"></i>
+          <span slot="title" style="font-size: 20px;">库存管理</span>
+        </el-menu-item>
+        <el-menu-item v-if="showPermissionsLink" index="2" @click="handleMenuClick('/dashboard/permissions')">
+          <i class="el-icon-lock"></i>
+          <span slot="title" style="font-size: 20px;">权限管理</span>
+        </el-menu-item>
+        <el-menu-item index="4" @click="handleMenuClick('/dashboard/notification')">
+          <i class="el-icon-bell" ></i>
+          <span slot="title" style="font-size: 20px;">通知中心</span>
+        </el-menu-item>
+      </el-menu>
+    </div>
     <!-- 主内容区域 -->
-    <div class="main-content">
-      <!-- 菜单栏 -->
-      <div class="sidebar">
-        <h1>菜单栏</h1>
-        <!-- 菜单链接 -->
-        <router-link to="/dashboard/myinfo" class="btn btn-primary">我的信息</router-link>
-        <!-- 根据权限显示/隐藏权限链接 -->
-        <router-link v-if="showPermissionsLink" to="/dashboard/permissions" class="btn btn-primary">权限管理</router-link>
-        <router-link to="/dashboard/inventory" class="btn btn-primary">库存管理</router-link>
-        <router-link to="/dashboard/notification" class="btn btn-primary">通知中心</router-link>
-      </div>
-      <!-- 主内容区域 -->
-      <div class="content">
-        <router-view></router-view>
-      </div>
+    <div class="content">
+      <router-view></router-view>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Dashboard',
   data() {
     return {
-      showPermissionsLink: false
+      showPermissionsLink: false, // 根据权限显示/隐藏权限链接
+      isCollapsed: false // 侧边栏是否折叠
     };
   },
+  methods: {
+    handleMenuClick(route) {
+      if (this.$route.path !== route) {
+        this.$router.push(route);
+      }
+    },
+  },
+  watch: {
+    $route(to) {
+      this.activeMenu = to.path;
+    }
+  },
+
   mounted() {
-    // 在页面加载时检查用户权限并设置是否显示权限链接
-    const userRole = sessionStorage.getItem("access_role"); // 角色信息存储在sessionStorage 中
+    //只有管理员才能看到权限管理菜单项
+    const userRole = sessionStorage.getItem("access_role");
     this.showPermissionsLink = userRole === 'admin';
   }
-}
+};
 </script>
 
 <style scoped>
-/* 整体布局容器样式 */
-.dashboard-container {
-  display: flex;
-  height: 100vh;
-  overflow: hidden;
-  position: relative;
-}
-
-/* 背景图片样式 */
-.background-image {
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background-image: url('https://source.unsplash.com/random/1920x1080');
-  background-size: cover;
-  background-position: center;
-  filter: blur(5px); /* 高斯模糊效果 */
-  z-index: -1; /* 将模糊背景置于容器和控件下方 */
-}
-
-/* 主内容区域样式 */
-.main-content {
-  display: flex;
-  flex: 1;
-}
-
-/* 菜单栏样式 */
 .sidebar {
-  padding: 20px;
-  background-color: rgba(255, 255, 255, 0.534); /* 菜单栏背景颜色 */
   display: flex;
-  flex-direction: column;
-  gap: 10px; /* 菜单项垂直间距 */
-  width: 15.5%;
+  position: fixed;
+  height: 100vh;
 }
 
-/* 主内容区域样式 */
-.content {
-  flex: 1;
-  padding: 20px;
-  background-color: rgba(255, 255, 255, 0.61); /* 主内容背景颜色 */
+.sidebar-content {
+  width: 14vw;
+  background-color: #f0f2f5;
+  color: #303133;
 }
 
-/* 按钮样式 */
-.btn {
-  border-radius: 10px; /* 圆角矩形按钮 */
-  background-color: #484b4e86;
-  color: white;
-  padding: 10px 20px;
-  transition: all 0.3s ease;
-  text-align: center;
+.sidebar-header {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-left: 10px;
+  padding: 40px;
+}
+
+.sidebar-logo {
+  width: 150px;
+  height: 50px;
+  margin-left: 10px;
+}
+
+.sidebar-title {
+  margin-left: 1vw;
+  font-size: 32px;
+}
+
+.el-menu-vertical-demo {
   border: none;
 }
 
-.btn:hover {
-  background-color: #7c96b14f;
+.el-menu-vertical-demo .el-menu-item,
+.el-menu-vertical-demo .el-submenu__title {
+  line-height: 9vh;
+  margin-bottom: 15px; /* 菜单项之间的间距 */
+}
+
+.el-menu-vertical-demo .el-menu-item:hover,
+.el-menu-vertical-demo .el-submenu.is-active .el-submenu__title {
+  background-color: #409eff;
+}
+
+.el-menu-vertical-demo .el-icon {
+  margin-right: 12px;
+}
+
+.el-menu-vertical-demo .el-menu-item.is-active {
+  background-color: #fff;
+}
+
+.content {
+  flex: 1;
+  padding: 20px;
 }
 </style>
+
