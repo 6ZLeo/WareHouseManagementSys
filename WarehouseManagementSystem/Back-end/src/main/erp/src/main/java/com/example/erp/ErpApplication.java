f79d1a28cd696c02d27aad929a031f52ee507a26
6ZLeo <136341102+6ZLeo@users.noreply.github.com>
2024-05-19 22:04:06 +0800

    测试在iteration3上commit

--- WarehouseManagementSystem/Back-end/src/main/erp/src/main/java/com/example/erp/ErpApplication.java
+++ WarehouseManagementSystem/Back-end/src/main/erp/src/main/java/com/example/erp/ErpApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ErpApplication {
-
+//测试在iteration3上commit
     public static void main(String[] args) {
         SpringApplication.run(ErpApplication.class, args);
     }
