0135c56c6789f25dfa06c61b569c2c37dbd80f7e
ZLeo <136341102+6ZLeo@users.noreply.github.com>
2024-06-11 13:02:34 +0800

    delete wrong things

--- WarehouseManagementSystem/Back-end/src/main/erp/erp/src/main/java/com/example/erp/service/LoginService.java
+++
@@ -1,8 +0,0 @@
-package com.example.erp.service;
-
-import com.example.erp.entity.User;
-import com.example.erp.utils.ResponseResult;
-
-public interface LoginService {
-    ResponseResult login(User user);
-}
